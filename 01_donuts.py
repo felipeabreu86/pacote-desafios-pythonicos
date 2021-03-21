58472f6e278a9c94f1ff482b3b1934993f9729ff
felipeabreu <felipeabreu.rj@gmail.com>
2021-03-21 11:18:42 -0300

    Se aplicado, este commit irá implementar novas regras de validação da função 'donuts': (1) teste se a quantidade é maior do que zero (2) teste se a quantidade é um numero inteiro.

--- 01_donuts.py
+++ 01_donuts.py
@@ -11,7 +11,12 @@ e donuts(23) retorna 'Number of donuts: many'
 
 
 def donuts(count):
-    return f'Number of donuts: {count if count < 10 else "many"}'
+    if not isinstance(count, int):
+        return 'Number of donuts must be an integer'
+    elif count < 0:
+        return 'Number of donuts must be greater than or equal to zero'
+    else:
+        return f'Number of donuts: {count if count < 10 else "many"}'
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
@@ -35,7 +40,11 @@ def test(f, in_, expected):
 
 if __name__ == '__main__':
     # Testes que verificam o resultado do seu código em alguns cenários.
+    test(donuts, -1, 'Number of donuts must be greater than or equal to zero')
+    test(donuts, 0, 'Number of donuts: 0')
     test(donuts, 4, 'Number of donuts: 4')
     test(donuts, 9, 'Number of donuts: 9')
     test(donuts, 10, 'Number of donuts: many')
     test(donuts, 99, 'Number of donuts: many')
+    test(donuts, 10.2, 'Number of donuts must be an integer')
+    test(donuts, 'test', 'Number of donuts must be an integer')
